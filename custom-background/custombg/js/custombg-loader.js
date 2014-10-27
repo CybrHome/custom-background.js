3d805535630ec142a6ede48ed8823ed4cbe0837b
Shubham Badal <shunty.fun@gmail.com>
2014-10-28 01:29:10 +0530

    improved

--- custom-background/custombg/js/custombg-loader.js
+++ custom-background/custombg/js/custombg-loader.js
@@ -21,46 +21,52 @@ function load_all()	{
 	var textNode =  document.createTextNode("#theme-div {position: fixed;z-index: -10;top: 0;left: 0;background-color: black;}#theme-div img.bgfade {position: absolute;top: 0;display: none;width: 100%;height: 100%; z-index: -100;}");
 	load4.appendChild(textNode);
 	$('head').append(load4);
-
-
-		 		
 }
+
 load_all();
 
-function loadOptionsWindow(window_path,event) {
-		
-		$("#options-window").load(window_path);  
 
-		  var load1 = document.createElement('link');
-		  load1.rel = 'stylesheet';
-		  load1.type = 'text/css';
-		  load1.href = 'custombg/css/options-window.css';
-		  $('head').append(load1);
-  
-		  var load2 = document.createElement('script');
-		  load2.type = 'text/javascript';
-		  load2.src ='custombg/js/components/jscolor.js';
-		  document.body.appendChild(load2);
-  
-		setTimeout(function(){
-			var load1 = document.createElement('script');
-			load1.type = 'text/javascript';
-			load1.src ='custombg/js/main/options-window.js';
-			document.getElementById('options-window').appendChild(load1);		
+function loadOptionsWindow(html_file_path,event) {
+		
+	$("#options-window").load(html_file_path);  
 	
-			setTimeout(function(){$("#options-window").css('display','block')},10);
-		},10)
+	var load1 = document.createElement('link');
+	load1.rel = 'stylesheet';
+	load1.type = 'text/css';
+	load1.href = 'custombg/css/options-window.css';
+	$('head').append(load1);
+	
+	var load2 = document.createElement('script');
+	load2.type = 'text/javascript';
+	load2.src ='custombg/js/components/jscolor.js';
+	document.body.appendChild(load2);
+	
+	setTimeout(function(){
+	var load1 = document.createElement('script');
+	load1.type = 'text/javascript';
+	load1.src ='custombg/js/main/options-window.js';
+	document.getElementById('options-window').appendChild(load1);		
+	setTimeout(function(){$("#options-window").css('display','block')},10);
+	},10)
+	
 }
-	function Close(){
-		$('#options-window').fadeOut();
-	}
+
+
+
+function Close(){
+	$('#options-window').fadeOut();
+}
+	
+
+
 $(document).click(function(){	
-	  $('html').click(function() {
-		 $('#options-window').hide(); 
-	  });
-	  
-	  $('#options-window').click(function(event){
-		   event.stopPropagation();
-	  });	
+
+  $('html').click(function() {
+	$('#options-window').hide(); 
+  });
+  
+  $('#options-window').click(function(event) {
+	event.stopPropagation();
+  });	
 			
 });
